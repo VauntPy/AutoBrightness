14a66c6164cc2d93eb24c0ab8d868b7cbac098bd
Vaunt.py <105758901+VauntPy@users.noreply.github.com>
2022-05-23 19:57:19 -0500

    Add files via upload

--- serialCom.py
+++ serialCom.py
@@ -1,6 +1,5 @@
 import subprocess
 import serial
-import time
 
 class SerialConn:
     def __init__(self, bRate: int, port: str):
@@ -13,9 +12,15 @@ class SerialConn:
             self.ser.baudrate = self.bRate
             self.ser.port = self.port
             self.ser.open()
-            # print(self.ser.isOpen())
+            print(self.ser.is_open)
         except serial.SerialException:
             print(f"Couldn't connect to Port {self.port}")
+
+    def readBytes(self, numBytes):
+        return self.ser.read(numBytes)
+
+    def sendBytes(self):
+        self.ser.write(b'Connection')
 
     def closeConn(self):
         self.ser.close()
@@ -29,9 +34,17 @@ class SerialConn:
 
 
 
-n = SerialConn.listPorts()
-# print(n)
+# n = SerialConn.listPorts()
+# sec = SerialConn(19200, n[0])
+# sec.setConn()
+# sec.sendBytes()
+# d = sec.readBytes(8)
+# print(d)
 
 
+# b = sec.readBytes(8)
+# b = b.decode()
+# print(b)
+# print(type(b))
 
 
