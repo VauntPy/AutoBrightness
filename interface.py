9eeb144eb8154acce85ce9550580954f8b8dad1b
Vaunt.py <105758901+VauntPy@users.noreply.github.com>
2022-05-27 23:49:26 -0500

    Add files via upload

--- interface.py
+++ interface.py
@@ -4,20 +4,30 @@ import requests
 # url = f"https://{SERVER}/external/api/get?token={TOKEN}&{VIRTUAL-PIN}"
 
 class Screen:
+    pVal = "100"
+    luxDic = {
+        10: 50,
+        40: 90,
+        65: 90,
+        100000: 100,
+    }
+
     def __init__(self, token, vPin) -> None:
         self.url = f"https://blynk.cloud/external/api/get?token={token}&{vPin}"
-        self.pVal = "100"
+     
     def setBrightness(self):
         self.cVal = self.getExternalLight()
         if self.cVal != self.pVal: 
-            sbc.set_brightness(self.cVal)
-            self.pVal = self.cVal
-            print(f"Setting Brightness to {self.pVal} %")     
+            for key,value in self.luxDic.items():
+                if int(self.cVal) <= key and self.getBrightness()[0] != value:
+                    sbc.set_brightness(value)
+                    self.pVal = self.cVal
+                    print(f"Brightness: {value} %")
+                    break
+
     def getExternalLight(self):
         return requests.get(self.url).text 
     @staticmethod
     def getBrightness():
         return sbc.get_brightness()
-
-
 
